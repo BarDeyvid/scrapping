8998756b386578fd1972a3de52b7f4898ff1db02
BarDeyvid <79212287+BarDeyvid@users.noreply.github.com>
2024-12-18 03:00:59 +0000

    Now it's a working scrapper that get the price and name from a product. But it does have a lot of work left!

---
+++ almost-scrapper.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+def data_scrape(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    products = []
+
+    for product in soup.find_all('section', class_='items-filters-section'):
+        name = product.find('a', class_='poly-component__title').text.strip() if product.find('a', class_='poly-component__title') else 'No Name' #It's using the type of text and his class to find the product, if doesn't have one it returns 'No Name'
+        price = product.find('span', class_='andes-money-amount andes-money-amount--cents-superscript').text.strip() if product.find('span', class_='andes-money-amount andes-money-amount--cents-superscript') else 'No Price' # Same kind of thing from above
+
+        products.append((name, price))
+        print(name, price, " were scrapped")
+    return products
+def create_database():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS database (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            price TEXT
+        )
+    ''')
+    print("Database Confirmed!")
+    conn.commit()
+    conn.close()
+
+def save_to_database(data):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    for name, price in data:
+        cursor.execute('INSERT INTO database (name, price) VALUES (?, ?)', (name, price)) # The command that makes the information go to the database
+
+    print("Name & Price Saved!")
+    conn.commit()
+    conn.close() 
+
+if __name__ == "__main__":
+    url = "https://www.mercadolivre.com.br/ofertas?container_id=MLB779362-1#filter_applied=container_id&filter_position=1&is_recommended_domain=false&origin=scut" # Replace with the target site
+    create_database()
+    database = data_scrape(url)
+    save_to_database(database)
+    print("Name and price data scraped and saved with code 1")
